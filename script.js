4ad687f191d8d7adb8d64612ab72252601369a3c
Leonardo <leonardo.roi3@gmail.com>
2022-03-17 16:06:14 +0100

    fixed bugs regarding div and mul

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Variable and constants declarations
+
 const numbers = document.querySelectorAll(".num");
 const clear_all = document.getElementsByClassName("clear-all")[0];
 const operators = document.querySelectorAll(".op");
@@ -12,30 +14,51 @@ let operator_exists = false;
 let equal_sign = document.getElementsByClassName("equal-sign")[0];
 let equaled = false;
 
-console.log(numbers);
-console.log(clear_all);
 
+/* Simple event listeners for the delete and clear button*/
+clear_all.addEventListener('click', clearEverything);
+delete_button.addEventListener('click',clearLastDigit);
+
+/* When equal button is clicked - render the final result, and reset other fields */ 
 equal_sign.addEventListener('click', () =>
 {
+  let result = 0;
+  if(factorNumber.innerText == "0")
+  {
+    last_operator_procedure = procedure.innerText.charAt(procedure.innerText.length-1);
+
+    if(procedure.innerText.includes("*") || procedure.innerText.includes("/"))
+    {
+      result = eval(procedure.innerText.slice(0,-1) + last_operator_procedure + resultNumber.innerText).toFixed(3);
+    }
+    else
+    {
+      result = eval(procedure.innerText.slice(0,-1) + last_operator_procedure + resultNumber.innerText)
+    }
+    clearEverything();
+    resultNumber.innerText = result;
+    equaled = true;
+    return;
+  }
+
   last_operator_procedure = procedure.innerText.charAt(procedure.innerText.length-1);
-  let result = eval(factorNumber.innerText + last_operator_procedure + resultNumber.innerText);
-  console.warn(last_operator_procedure);
+   result = eval(factorNumber.innerText + last_operator_procedure + resultNumber.innerText).toFixed(3);
   clearEverything();
   resultNumber.innerText = result;
   equaled = true;
-  result = 0;
 })
 
+/* numbers have similar behaviours, so a forEach was used, different scenarios are specified further */
 numbers.forEach((number) => 
 {
-
-  if(equaled === true)
-  {
-    resultNumber.innerText = "";
-    equaled = false;
-  }
     number.addEventListener('click', ()=> 
     {
+      if(equaled === true)
+      {
+        resultNumber.innerText = "";
+        equaled = false;
+      }
+
       if(operator_clicked === true)
       {
         operator_clicked = false;
@@ -49,58 +72,101 @@ operators.forEach((operator)=>
 {
   operator.addEventListener("click", ()=>
   {
-
     last_operator = operator.innerHTML.toString();
-
     if(resultNumber.innerText == "0") // checks if operand is null
     {
       procedure.innerText = procedure.innerText.slice(0,-1);
       procedure.innerText += last_operator;
       return;
     }
-
-    let operand = resultNumber.innerHTML;
-    if(last_operator == "*" || last_operator =="÷")
+    console.log(last_operator);
+    if(last_operator == "*" || last_operator == "/")
     {
-      last_operator_procedure = procedure.innerText.charAt(procedure.innerText.length-1);
-      factorNumber.innerHTML = eval(factorNumber.innerText + last_operator_procedure + resultNumber.innerText);
-      if(resultNumber.innerText !== "0")
+
+      if(factorNumber.innerText != "0")
       {
-        procedure.innerText += operand + last_operator; 
+        console.warn("hit_1");
+        procedure.innerText += resultNumber.innerText + last_operator;  
+        factorNumber.innerText = eval(procedure.innerText.slice(0,-1)).toFixed(3);
+        resetResultNumber();
+        return;
       }
-       resetResultNumber();
-       return;
+
+      if(procedure.innerText !== "" && resultNumber.innerText!=="0" && factorNumber.innerText=="0")
+      {
+        console.warn("4")
+        procedure.innerText += resultNumber.innerText;
+        factorNumber.innerText = eval(procedure.innerText);
+        procedure.innerText += last_operator;
+        resetResultNumber();
+        return;
+      }
+
+      if(procedure.innerText == "")
+      {
+        console.warn("hit_2");
+        procedure.innerText +=  resultNumber.innerText + last_operator; 
+      
+        resetResultNumber();
+        return;
+      }
+
+      else
+      {
+        console.warn("hit_3");
+        if(procedure.innerText.includes("-") || procedure.innerText.includes("+") )
+        {
+          factorNumber.innerText = eval(factorNumber.innerText + last_operator + resultNumber.innerText);
+        }
+        else
+        {
+          factorNumber.innerText = eval(procedure.innerText + resultNumber.innerText).toFixed(3);
+        }
+        procedure.innerText += resultNumber.innerText + last_operator;
+        resetResultNumber();
+        return;
+      }
     } 
 
-    factorNumber.innerHTML = eval(factorNumber.innerText + last_operator + resultNumber.innerText);
-    operator_clicked = true;
-    if(resultNumber.innerText !== "0")
+
+    let expression = eval(resultNumber.innerText + last_operator + "0");
+    if(procedure.innerText == "")
     {
-      procedure.innerText += operand + last_operator;  
+      procedure.innerText += expression + last_operator;
+      resetResultNumber();
+      return;
     }
+
+    if(factorNumber.innerText == "0")
+    {
+      factorNumber.innerText = eval(procedure.innerText + resultNumber.innerText).toFixed(4);
+      procedure.innerText += resultNumber.innerText + last_operator;
+      resetResultNumber();
+      return;
+    }
+
+    factorNumber.innerText = eval(procedure.innerText + resultNumber.innerText).toFixed(3);
+    procedure.innerText += resultNumber.innerText + last_operator;
     resetResultNumber();
+    return;
   });
 });
 
-clear_all.addEventListener('click', clearEverything);
-delete_button.addEventListener('click',clearLastDigit);
-
+// Renders a number
 function updateNumber(lastClickedNumber)
 {
-
   if (checkValidity(lastClickedNumber) === false)
     return;
 
-    if(resultNumber.innerText == "0" && lastClickedNumber !==".")
+  if(resultNumber.innerText == "0" && lastClickedNumber !==".")
     resultNumber.innerHTML = "";
 
-    resultNumber.innerHTML += lastClickedNumber;
-
+  resultNumber.innerHTML += lastClickedNumber;
 }
 
+// Prevents multiple "0" and "." values 
 function checkValidity(lastClickedNumber)
 {
-
   if(resultNumber.innerText.includes("0") && resultNumber.innerText.includes("."))
   {
     if(lastClickedNumber==".")
@@ -122,7 +188,6 @@ function checkValidity(lastClickedNumber)
     if(resultNumber.innerHTML.includes('.'))
       return false;
   }
-
   return true;
 }
 
@@ -140,9 +205,11 @@ function clearEverything()
 {
   resetFactorNumber();
   resetResultNumber();
-   procedure.innerHTML = "";
+  procedure.innerHTML = ""; // one instance, no function required
+   result = 0;
 }
 
+// this function is called by delete event listener
 function clearLastDigit()
 {
   if (resultNumber.innerText == "0")
